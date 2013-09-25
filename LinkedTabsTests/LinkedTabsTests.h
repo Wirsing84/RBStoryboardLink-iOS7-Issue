74acb246c030c42a3589cb007385d5378f7521da
Chris Chard <chris@foodque.st>
2013-09-25 18:37:21 +0200

    second commit

---
+++ LinkedTabsTests/LinkedTabsTests.h
@@ -0,0 +1,13 @@
+//
+//  LinkedTabsTests.h
+//  LinkedTabsTests
+//
+//  Created by Robert Brown on 6/10/12.
+//  Copyright (c) 2012-2013 Robert Brown. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LinkedTabsTests : SenTestCase
+
+@end
